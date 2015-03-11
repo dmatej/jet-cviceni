7c7e72eccaac9712432a2b2ad5ab23bb28997f00
David Matejcek <dmatej@seznam.cz>
2015-03-11 22:21:48 +0100

    Fixed unit test - obtained new collection

--- jet-utils/src/test/java/cz/zcu/jet/util/CollectionUtilsTest.java
+++ jet-utils/src/test/java/cz/zcu/jet/util/CollectionUtilsTest.java
@@ -1,6 +1,7 @@
 package cz.zcu.jet.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
@@ -19,17 +20,16 @@ public class CollectionUtilsTest {
 
   @Test
   public void sortOnlyNullInList() {
-    final Collection<String> names = Arrays.asList((String)null);
+    final Collection<String> names = Arrays.asList((String) null);
     final List<String> sorted = CollectionUtils.sort(names);
     assertNotNull("null", sorted);
     assertEquals("sorted.size", 1, sorted.size());
     assertNull("sorted[0]", sorted.get(0));
   }
 
-
   @Test
   public void sortNames() {
-    final Collection<String> names = Arrays.asList("řízeček", null, "matějček", "ČOČKA");
+    final List<String> names = Arrays.asList("řízeček", null, "matějček", "ČOČKA");
     final List<String> sorted = CollectionUtils.sort(names);
     assertNotNull("null", sorted);
     assertEquals("sorted.size", 4, sorted.size());
@@ -37,6 +37,8 @@ public class CollectionUtilsTest {
     assertEquals("sorted[1]", "ČOČKA", sorted.get(1));
     assertEquals("sorted[2]", "matějček", sorted.get(2));
     assertEquals("sorted[3]", "řízeček", sorted.get(3));
-    assertEquals("sorted.hashcode and names.hashcode", names.hashCode(), sorted.hashCode());
+    assertFalse("sorted.hashcode and names.hashcode should not be equal",
+        names.hashCode() == sorted.hashCode());
+    assertEquals("null changed position in original list!", null, names.get(1));
   }
 }
