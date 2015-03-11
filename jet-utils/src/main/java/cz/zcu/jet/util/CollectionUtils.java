49beb9a223a25f35b7096effa31e53108e0215ec
David Matejcek <dmatej@seznam.cz>
2015-03-11 23:17:56 +0100

    Evil ugly method

--- jet-utils/src/main/java/cz/zcu/jet/util/CollectionUtils.java
+++ jet-utils/src/main/java/cz/zcu/jet/util/CollectionUtils.java
@@ -1,5 +1,9 @@
 package cz.zcu.jet.util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.Collator;
 import java.util.Collection;
 import java.util.List;
@@ -7,9 +11,12 @@ import java.util.Locale;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 /**
  * Utilities to simplify a work with collections.
@@ -34,6 +41,18 @@ public class CollectionUtils {
     return words.stream()
         .sorted((a, b) -> collator.compare(Objects.toString(a, ""), Objects.toString(b, "")))
         .collect(Collectors.toList());
+  }
+
+  public static void JDBCbastlFujTajxl() throws NamingException, SQLException {
+    final InitialContext ctx = new InitialContext();
+    final DataSource ds = (DataSource) ctx.lookup("jdbc/jetDb");
+    final Connection connection = ds.getConnection();
+    final PreparedStatement statement = connection
+        .prepareStatement("select everything from database " + 256);
+    final ResultSet resultSet = statement.executeQuery();
+    if (resultSet.next()) {
+      throw new RuntimeException();
+    }
   }
 
 }
