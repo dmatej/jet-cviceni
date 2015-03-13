31565ff5f1a244dbb811bf7f4c20d4c3458977f2
David Matejcek <dmatej79@gmail.com>
2015-03-13 12:20:18 +0100

    Fixed ejb module
    - it must have at least one bean
    - webservices should be stateless beans
    - ejb-api dependency has provided scope because is always present in JEE container's classpath

--- jet-webservice/src/main/java/cz/zcu/jet/ws/soap/HelloImpl.java
+++ jet-webservice/src/main/java/cz/zcu/jet/ws/soap/HelloImpl.java
@@ -1,11 +1,13 @@
 package cz.zcu.jet.ws.soap;
 
+import javax.ejb.Stateless;
 import javax.jws.WebService;
 
 @WebService(
     endpointInterface = "cz.zcu.jet.ws.soap.Hello",
     serviceName = "HelloService",
     portName = "HelloServicePortType")
+@Stateless
 public class HelloImpl {
   public String sayHello(final String name) {
     return "Hello, " + name + ".";
